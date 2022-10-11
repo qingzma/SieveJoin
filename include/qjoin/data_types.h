8e1ea439802dd18a4efbb733e53690b3149f9757
quincyma <quincyma@tencent.com>
2022-10-11 14:17:50 +0800

    add table header

--- include/qjoin/data_types.h
+++ include/qjoin/data_types.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef QJOIN_INCLUDE_DATA_TYPE_H_
+#define QJOIN_INCLUDE_DATA_TYPE_H_
 #include <cstdint>
 enum DATABASE_DATA_TYPES {
   INT32,
@@ -14,3 +15,5 @@ constexpr const char COL_DELIMITER = '|';
 
 typedef int64_t db_key_t_;
 typedef int64_t db_time_t_;
+
+#endif  // QJOIN_INCLUDE_DATA_TYPE_H_
