2eeaa157acb42e307e3548f6b3f7b883d943c2be
quincyma <quincyma@tencent.com>
2022-10-18 20:10:42 +0800

    qx loop join

--- db/qx.cpp
+++ db/qx.cpp
@@ -1,5 +1,6 @@
 #include "qx.h"
 
+#include <fstream>
 #include <iostream>
 #include <memory>
 
@@ -40,7 +41,7 @@ void Qx(Options& options) {
   //   JoinedTableLists table_lists = {tbl_nation_, tbl_supplier_,
   //   tbl_customer_,
   //                                   tbl_orders_, tbl_lineitem_};
-
+  //
   //   // create the join plan
   //   ColIsSelected colIsSelected = {};
   //   ColIsJoined colIsJoined = {};
@@ -63,8 +64,59 @@ void Qx(Options& options) {
   //   CyclicLoopJoin loop_join;
   //   loop_join.Join(&options, table_lists, colIsJoined, colIsSelected);
 
-  for (auto iter = tbl_nation_->KeyIterator(0);
-       iter < tbl_nation_->KeyIterator(0) + tbl_nation_.)
-    std::cout << "done with query X" << std::endl;
+#ifdef BOOL_WRITE_JOIN_RESULT_TO_FILE
+  std::ofstream myfile(options.path_prefix + "qx.txt");
+#endif  // BOOL_WRITE_JOIN_RESULT_TO_FILE
+
+  int64_t join_cnt = 0;
+  // loop nation
+  for (int64_t i0 = 0; i0 < tbl_nation_->Size(); i0++) {
+    db_key_t_ n_nation = tbl_nation_->col0_->at(i0);
+
+    // loop supplier
+    for (int64_t i1 = 0; i1 < tbl_supplier_->Size(); i1++) {
+      db_key_t_ s_nation = tbl_supplier_->col0_->at(i1);
+      db_key_t_ s_supp = tbl_supplier_->col1_->at(i1);
+      if (n_nation == s_nation) {
+        // loop customer
+        for (int64_t i2 = 0; i2 < tbl_customer_->Size(); i2++) {
+          db_key_t_ c_nation = tbl_customer_->col0_->at(i2);
+          db_key_t_ c_cust = tbl_customer_->col1_->at(i2);
+          if (s_nation == c_nation) {
+            // loop orders
+            for (int64_t i3 = 0; i3 < tbl_orders_->Size(); i3++) {
+              db_key_t_ o_cust = tbl_orders_->col0_->at(i3);
+              db_key_t_ o_order = tbl_orders_->col1_->at(i3);
+              if (c_cust == o_cust) {
+                // loop lineitem
+                for (int64_t i4 = 0; i4 < tbl_lineitem_->Size(); i4++) {
+                  db_key_t_ l_order = tbl_lineitem_->col0_->at(i4);
+                  db_key_t_ l_linenum = tbl_lineitem_->col1_->at(i4);
+                  if (o_order == l_order) {
+                    join_cnt++;
+                    if (join_cnt % 10000 == 0)
+                      std::cout << "find " << join_cnt << " results"
+                                << std::endl;
+#ifdef BOOL_WRITE_JOIN_RESULT_TO_FILE
+                    myfile << n_nation << "," << s_supp << "," << c_cust << ","
+                           << o_order << "," << l_linenum << "\n";
+                    if (join_cnt % 10000 == 0) myfile.flush();
+#endif  // BOOL_WRITE_JOIN_RESULT_TO_FILE
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+#ifdef BOOL_WRITE_JOIN_RESULT_TO_FILE
+  myfile.close();
+#endif  // BOOL_WRITE_JOIN_RESULT_TO_FILE
+
+  std::cout << "join size is " << join_cnt << std::endl;
+  std::cout << "done with query X" << std::endl;
 }
 }  // namespace qjoin
