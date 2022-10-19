2411eb539ea08a8dda96af76aad764aa2d7ee426
Quincy Ma <quincyma@tencent.com>
2022-10-19 20:24:13 +0800

    save query x, find good experiment

--- db/qx.cpp
+++ db/qx.cpp
@@ -1,5 +1,6 @@
 #include "qx.h"
 
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <memory>
@@ -12,8 +13,14 @@
 #include "qjoin/table_impl.h"
 
 namespace qjoin {
+void QueryX::resetCounter() {
+  n_access_tuple_ = 0;
+  n_access_index_ = 0;
+  n_access_bf_ = 0;
+}
 
 QueryX::QueryX(Options& options) : options_(options) {
+  resetCounter();
   // load data
   tbl_nation_ =
       std::make_shared<TableImpl>(options, options_.path_prefix + "nation.tbl",
@@ -28,7 +35,7 @@ QueryX::QueryX(Options& options) : options_(options) {
       options, options_.path_prefix + "orders.tbl", '|', O_CUSTKEY,
       O_ORDERKEY);  // 1, 0
   tbl_lineitem_ = std::make_shared<TableImpl>(
-      options, options_.path_prefix + "lineitem.tbl", '|', L_ORDERKEY,
+      options, options_.path_prefix + "lineitem_6w.tbl", '|', L_ORDERKEY,
       L_LINENUMBER);  // 0, 3
 
   // build indexes
@@ -124,6 +131,7 @@ void QueryX::LoopJoin() {
 void QueryX::IndexJoin() {
   std::cout << "--------------------------------------------" << std::endl;
   std::cout << "Index join starts for query x." << std::endl;
+  std::chrono::steady_clock::time_point t0 = std::chrono::steady_clock::now();
 
 #ifdef BOOL_WRITE_JOIN_RESULT_TO_FILE
   std::ofstream index_join_file(options_.path_prefix + "qx_index.txt");
@@ -133,32 +141,41 @@ void QueryX::IndexJoin() {
   // loop nation
 
   for (db_key_t_ n_nation : *(tbl_nation_->col0_)) {
+    n_access_tuple_++;
     // check existence and loop supplier
+    n_access_index_++;
     auto s_nation_iter = tbl_supplier_->col0_index_->equal_range(n_nation);
     for (auto iter_s = s_nation_iter.first; iter_s != s_nation_iter.second;
          iter_s++) {
       db_key_t_ s_nation = iter_s->first;
       int64_t s_idx = iter_s->second;
+      n_access_tuple_++;
       db_key_t_ s_supp = tbl_supplier_->col1_->at(s_idx);
       // looop customer
+      n_access_index_++;
       auto c_nation_iter = tbl_customer_->col0_index_->equal_range(s_nation);
       for (auto iter_c = c_nation_iter.first; iter_c != c_nation_iter.second;
            iter_c++) {
         db_key_t_ c_nation = iter_c->first;
         int64_t c_idx = iter_c->second;
+        n_access_tuple_++;
         db_key_t_ c_cust = tbl_customer_->col1_->at(c_idx);
         // loop orders
+        n_access_index_++;
         auto o_cust_iter = tbl_orders_->col0_index_->equal_range(c_cust);
         for (auto iter_o = o_cust_iter.first; iter_o != o_cust_iter.second;
              iter_o++) {
           db_key_t_ o_cust = iter_o->first;
           int64_t o_idx = iter_o->second;
+          n_access_tuple_++;
           db_key_t_ o_order = tbl_orders_->col1_->at(o_idx);
           // loop lineitem
+          n_access_index_++;
           auto l_order_iter = tbl_lineitem_->col0_index_->equal_range(o_order);
           for (auto iter_l = l_order_iter.first; iter_l != l_order_iter.second;
                iter_l++) {
             int64_t l_idx = iter_l->second;
+            n_access_tuple_++;
             db_key_t_ l_linenum = tbl_lineitem_->col1_->at(l_idx);
             join_cnt++;
             if (join_cnt % 10000 == 0)
@@ -177,6 +194,16 @@ void QueryX::IndexJoin() {
 #ifdef BOOL_WRITE_JOIN_RESULT_TO_FILE
   index_join_file.close();
 #endif  // BOOL_WRITE_JOIN_RESULT_TO_FILE
+
+  std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+  std::cout << "time cost: "
+            << std::chrono::duration_cast<std::chrono::duration<double>>(t1 -
+                                                                         t0)
+                   .count()
+            << std::endl;
+
+  std::cout << "access tuples: " << n_access_tuple_ << std::endl;
+  std::cout << "access indexed: " << n_access_index_ << std::endl;
 
   std::cout << "Index join ends for query x with join size: " << join_cnt
             << std::endl;
@@ -185,41 +212,61 @@ void QueryX::IndexJoin() {
 void QueryX::QIndexJoin() {
   std::cout << "--------------------------------------------" << std::endl;
   std::cout << "QIndexJoin starts for query x." << std::endl;
+  std::chrono::steady_clock::time_point t0 = std::chrono::steady_clock::now();
 
 #ifdef BOOL_WRITE_JOIN_RESULT_TO_FILE
-  std::ofstream index_join_file(options_.path_prefix + "qx_index.txt");
+  std::ofstream index_join_file(options_.path_prefix + "qx_qindex_join.txt");
 #endif  // BOOL_WRITE_JOIN_RESULT_TO_FILE
 
   int64_t join_cnt = 0;
   // loop nation
 
   for (db_key_t_ n_nation : *(tbl_nation_->col0_)) {
+    n_access_tuple_++;
     // check existence and loop supplier
+    n_access_bf_++;
+    if (!tbl_nation_->col0_bf_->bf_.contains(n_nation))
+      continue;  // skip if not exist in current table
+    n_access_index_++;
     auto s_nation_iter = tbl_supplier_->col0_index_->equal_range(n_nation);
     for (auto iter_s = s_nation_iter.first; iter_s != s_nation_iter.second;
          iter_s++) {
       db_key_t_ s_nation = iter_s->first;
       int64_t s_idx = iter_s->second;
+      n_access_tuple_++;
       db_key_t_ s_supp = tbl_supplier_->col1_->at(s_idx);
       // looop customer
+      n_access_bf_++;
+      if (!tbl_supplier_->col0_bf_->bf_.contains(s_nation))
+        continue;  // skip if not exist in current table
+      n_access_index_++;
       auto c_nation_iter = tbl_customer_->col0_index_->equal_range(s_nation);
       for (auto iter_c = c_nation_iter.first; iter_c != c_nation_iter.second;
            iter_c++) {
         db_key_t_ c_nation = iter_c->first;
         int64_t c_idx = iter_c->second;
+        n_access_tuple_++;
         db_key_t_ c_cust = tbl_customer_->col1_->at(c_idx);
         // loop orders
+        n_access_bf_++;
+        if (!tbl_customer_->col1_bf_->bf_.contains(c_cust)) continue;
+        n_access_index_++;
         auto o_cust_iter = tbl_orders_->col0_index_->equal_range(c_cust);
         for (auto iter_o = o_cust_iter.first; iter_o != o_cust_iter.second;
              iter_o++) {
           db_key_t_ o_cust = iter_o->first;
           int64_t o_idx = iter_o->second;
+          n_access_tuple_++;
           db_key_t_ o_order = tbl_orders_->col1_->at(o_idx);
+          n_access_bf_++;
+          if (!tbl_orders_->col1_bf_->bf_.contains(o_order)) continue;
           // loop lineitem
+          n_access_index_++;
           auto l_order_iter = tbl_lineitem_->col0_index_->equal_range(o_order);
           for (auto iter_l = l_order_iter.first; iter_l != l_order_iter.second;
                iter_l++) {
             int64_t l_idx = iter_l->second;
+            n_access_tuple_++;
             db_key_t_ l_linenum = tbl_lineitem_->col1_->at(l_idx);
             join_cnt++;
             if (join_cnt % 10000 == 0)
@@ -238,6 +285,16 @@ void QueryX::QIndexJoin() {
 #ifdef BOOL_WRITE_JOIN_RESULT_TO_FILE
   index_join_file.close();
 #endif  // BOOL_WRITE_JOIN_RESULT_TO_FILE
+
+  std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+  std::cout << "time cost: "
+            << std::chrono::duration_cast<std::chrono::duration<double>>(t1 -
+                                                                         t0)
+                   .count()
+            << std::endl;
+  std::cout << "access tuples: " << n_access_tuple_ << std::endl;
+  std::cout << "access indexed: " << n_access_index_ << std::endl;
+  std::cout << "access bfs: " << n_access_bf_ << std::endl;
 
   std::cout << "QIndexJoin ends for query x with join size: " << join_cnt
             << std::endl;
