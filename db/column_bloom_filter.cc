4ad5e07f30285fe26ac13da4283de3e2b9686ec5
Quincy Ma <quincyma@tencent.com>
2022-10-19 13:22:01 +0800

    add bloom filter support

---
+++ db/column_bloom_filter.cc
@@ -0,0 +1,57 @@
+#include "qjoin/column_bloom_filter.h"
+
+#include <iostream>
+namespace qjoin {
+ColumnBloomFilter::ColumnBloomFilter(Options& options, int64_t col_size) {
+  // How many elements roughly do we expect to insert?
+  parameters_.projected_element_count = col_size;
+
+  // Maximum tolerable false positive probability? (0,1)
+  parameters_.false_positive_probability = 0.001;  // 1 in 1000
+
+  // Simple randomizer (optional)
+  //   parameters_.random_seed = 0xA5A5A5A5;
+
+  if (!parameters_) {
+    std::cout << "Error - Invalid set of bloom filter parameters!" << std::endl;
+    return;
+  }
+
+  parameters_.compute_optimal_parameters();
+
+  // Instantiate Bloom Filter
+  bf = std::make_shared<bloom_filter>(parameters_);
+
+  level_ = 0;
+}
+
+void ColumnBloomFilter::Fit(std::shared_ptr<std::vector<db_key_t_>>& column) {
+  for (auto val : *column) bf->insert(val);
+}
+
+void ColumnBloomFilter::UpdateBfFromOutsideColumn(
+    const std::shared_ptr<std::vector<db_key_t_>>& column,
+    const ColumnBloomFilter& cbf) {
+  // reset Bloom Filter
+  bf->clear();
+
+  for (auto val : *column) {
+    if (cbf.bf->contains(val)) bf->insert(val);
+  }
+}
+
+void ColumnBloomFilter::UpdateBfFromInsideColumn(
+    const std::shared_ptr<std::vector<db_key_t_>>& dest,
+    const std::shared_ptr<std::vector<db_key_t_>>& source,
+    const ColumnBloomFilter& bf_source) {
+  // reset Bloom Filter
+  bf->clear();
+
+  for (int64_t idx = 0; idx < dest->size(); idx++) {
+    if (bf_source.bf->contains(source->at(idx))) {
+      bf->insert(dest->at(idx));
+    }
+  }
+}
+
+}  // namespace qjoin
---
+++ include/qjoin/column_bloom_filter.h
@@ -0,0 +1,38 @@
+#ifndef QJOIN_INCLUDE_QJOIN_COLUMN_BLOOM_FILTER_H_
+#define QJOIN_INCLUDE_QJOIN_COLUMN_BLOOM_FILTER_H_
+
+#include <memory>
+
+#include "util/bloom_filter.h"
+
+#include "qjoin/data_types.h"
+#include "qjoin/options.h"
+
+namespace qjoin {
+class ColumnBloomFilter {
+ private:
+  int level_;
+  bloom_parameters parameters_;
+
+ public:
+  std::shared_ptr<bloom_filter> bf;
+
+ public:
+  ColumnBloomFilter(Options& options, int64_t col_size);
+
+  void Fit(std::shared_ptr<std::vector<db_key_t_>>& column);
+
+  // merge the bf on column b_1 in table 1  with equal condition on column b_2
+  // from another table b2.
+  void UpdateBfFromOutsideColumn(
+      const std::shared_ptr<std::vector<db_key_t_>>& column,
+      const ColumnBloomFilter& cbf);
+
+  void UpdateBfFromInsideColumn(
+      const std::shared_ptr<std::vector<db_key_t_>>& dest,
+      const std::shared_ptr<std::vector<db_key_t_>>& source,
+      const ColumnBloomFilter& bf_source);
+};
+}  // namespace qjoin
+
+#endif  // QJOIN_INCLUDE_QJOIN_COLUMN_BLOOM_FILTER_H_
