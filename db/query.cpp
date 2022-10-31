854d60942d15295a311930a1800a6150b2b14b1b
quincyma <quincyma@tencent.com>
2022-10-31 11:03:36 +0800

    add query.h

---
+++ db/query.cpp
@@ -0,0 +1,15 @@
+#include "query.h"
+
+namespace qjoin {
+void Query::resetCounter() {
+  n_access_tuple_ = 0;
+  n_access_index_ = 0;
+  n_access_bf_ = 0;
+}
+
+// void Query::Run() { throw std::runtime_error("not implemented."); }
+void Query::LoopJoin() { throw std::runtime_error("not implemented."); }
+void Query::IndexJoin() { throw std::runtime_error("not implemented."); }
+void Query::QLoopJoin() { throw std::runtime_error("not implemented."); }
+void Query::QIndexJoin() { throw std::runtime_error("not implemented."); }
+}  // namespace qjoin
