f9b286ac2f131b938d35389c621fedd0831319b1
quincyma <quincyma@tencent.com>
2022-11-02 09:55:11 +0800

    lineitem sample generation

---
+++ data/tpch/generate_percent_data.sh
@@ -0,0 +1,13 @@
+head -n 480457 x0.1/lineitem.tbl > x0.1/lineitem_80p.tbl
+head -n 360343 x0.1/lineitem.tbl > x0.1/lineitem_60p.tbl
+head -n 240228 x0.1/lineitem.tbl > x0.1/lineitem_40p.tbl
+head -n 120114 x0.1/lineitem.tbl > x0.1/lineitem_20p.tbl
+head -n 60057 x0.1/lineitem.tbl > x0.1/lineitem_10p.tbl
+head -n 6005 x0.1/lineitem.tbl > x0.1/lineitem_1p.tbl
+
+head -n 4800972 x1/lineitem.tbl > x1/lineitem_80p.tbl
+head -n 3600729 x1/lineitem.tbl > x1/lineitem_60p.tbl
+head -n 2400486 x1/lineitem.tbl > x1/lineitem_40p.tbl
+head -n 1200243 x1/lineitem.tbl > x1/lineitem_20p.tbl
+head -n 600121 x1/lineitem.tbl > x1/lineitem_10p.tbl
+head -n 60012 x1/lineitem.tbl > x1/lineitem_1p.tbl
