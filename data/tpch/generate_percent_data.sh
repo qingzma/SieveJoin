5dfe33f505507a20f25231edeae8a8438d3dc222
Quincy Ma <quincyma@tencent.com>
2022-11-10 14:55:11 +0800

    save

--- data/tpch/generate_percent_data.sh
+++ data/tpch/generate_percent_data.sh
@@ -4,6 +4,7 @@ head -n 240228 x0.1/lineitem.tbl > x0.1/lineitem_40p.tbl
 head -n 120114 x0.1/lineitem.tbl > x0.1/lineitem_20p.tbl
 head -n 60057 x0.1/lineitem.tbl > x0.1/lineitem_10p.tbl
 head -n 6005 x0.1/lineitem.tbl > x0.1/lineitem_1p.tbl
+head -n 600 x0.1/lineitem.tbl > x0.1/lineitem_0.1p.tbl
 
 head -n 4800972 x1/lineitem.tbl > x1/lineitem_80p.tbl
 head -n 3600729 x1/lineitem.tbl > x1/lineitem_60p.tbl
@@ -11,3 +12,4 @@ head -n 2400486 x1/lineitem.tbl > x1/lineitem_40p.tbl
 head -n 1200243 x1/lineitem.tbl > x1/lineitem_20p.tbl
 head -n 600121 x1/lineitem.tbl > x1/lineitem_10p.tbl
 head -n 60012 x1/lineitem.tbl > x1/lineitem_1p.tbl
+head -n 6001 x1/lineitem.tbl > x1/lineitem_0.1p.tbl
