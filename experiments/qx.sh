a8aac7428ae8e222b0fb9a3bb5f0a74274617294
Qingzhi Ma <qingzma@gmail.com>
2023-02-09 13:04:15 +0800

    save experiments on ubuntu systems

---
+++ experiments/qx.sh
@@ -0,0 +1,35 @@
+./qentry -x -p ../data/tpch/x0.1/  -k _0.1p --qindexjoin >> qx/log.txt
+./qentry -x -p ../data/tpch/x0.1/  -k _1p --qindexjoin >> qx/log.txt
+./qentry -x -p ../data/tpch/x0.1/  -k _10p --qindexjoin >> qx/log.txt
+./qentry -x -p ../data/tpch/x0.1/  -k _20p --qindexjoin >> qx/log.txt
+./qentry -x -p ../data/tpch/x0.1/  -k _40p --qindexjoin >> qx/log.txt
+./qentry -x -p ../data/tpch/x0.1/  -k _60p --qindexjoin >> qx/log.txt
+./qentry -x -p ../data/tpch/x0.1/  -k _80p --qindexjoin >> qx/log.txt
+./qentry -x -p ../data/tpch/x0.1/  --qindexjoin >> qx/log.txt
+
+./qentry -x -p ../data/tpch/x0.1/   -k _0.1p --index >> qx/log.txt
+./qentry -x -p ../data/tpch/x0.1/   -k _1p --index >> qx/log.txt
+./qentry -x -p ../data/tpch/x0.1/   -k _10p --index >> qx/log.txt
+./qentry -x -p ../data/tpch/x0.1/   -k _20p --index >> qx/log.txt
+./qentry -x -p ../data/tpch/x0.1/   -k _40p --index >> qx/log.txt
+./qentry -x -p ../data/tpch/x0.1/   -k _60p --index >> qx/log.txt
+./qentry -x -p ../data/tpch/x0.1/   -k _80p --index >> qx/log.txt
+./qentry -x -p ../data/tpch/x0.1/   --index >> qx/log.txt
+
+./qentry -x -p ../data/tpch/x1/  -k _0.1p --qindexjoin >> qx/log.txt
+./qentry -x -p ../data/tpch/x1/  -k _1p --qindexjoin >> qx/log.txt
+./qentry -x -p ../data/tpch/x1/  -k _10p --qindexjoin >> qx/log.txt
+./qentry -x -p ../data/tpch/x1/  -k _20p --qindexjoin >> qx/log.txt
+./qentry -x -p ../data/tpch/x1/  -k _40p --qindexjoin >> qx/log.txt
+./qentry -x -p ../data/tpch/x1/  -k _60p --qindexjoin >> qx/log.txt
+./qentry -x -p ../data/tpch/x1/  -k _80p --qindexjoin >> qx/log.txt
+./qentry -x -p ../data/tpch/x1/  --qindexjoin >> qx/log.txt
+
+./qentry -x -p ../data/tpch/x1/   -k _0.1p --index >> qx/log.txt
+./qentry -x -p ../data/tpch/x1/   -k _1p --index >> qx/log.txt
+./qentry -x -p ../data/tpch/x1/   -k _10p --index >> qx/log.txt
+./qentry -x -p ../data/tpch/x1/   -k _20p --index >> qx/log.txt
+./qentry -x -p ../data/tpch/x1/   -k _40p --index >> qx/log.txt
+./qentry -x -p ../data/tpch/x1/   -k _60p --index >> qx/log.txt
+./qentry -x -p ../data/tpch/x1/   -k _80p --index >> qx/log.txt
+./qentry -x -p ../data/tpch/x1/   --index >> qx/log.txt
